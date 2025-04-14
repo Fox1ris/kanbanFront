67f247d395d31eaba1511331b0c7bd4c18bcb2e8
Foxiris <kirill-2202@yandex.ru>
2025-04-14 13:22:56 +0300

    Tasks rdy

--- kanban/src/app/kanban/componenst/KanbanBoard.tsx
+++ kanban/src/app/kanban/componenst/KanbanBoard.tsx
@@ -1,22 +1,26 @@
 'use client';
 
 import {useEffect, useState} from 'react';
-import { DndContext, DragEndEvent, closestCorners } from '@dnd-kit/core';
-import { SortableContext, arrayMove } from '@dnd-kit/sortable';
+import {DndContext, DragEndEvent, closestCorners} from '@dnd-kit/core';
+import {SortableContext, arrayMove} from '@dnd-kit/sortable';
 import KanbanColumn from './KanbanColumn';
-import { PlusIcon } from '@heroicons/react/24/outline';
+import {PlusIcon} from '@heroicons/react/24/outline';
+import axios from "axios";
 
 type Task = {
     id: number;
     title: string;
     description: string;
-    status: 'todo' | 'in-progress' | 'done';
+    status: 'todo' | 'in_progress' | 'done';
     project: number;
 };
 
 export default function KanbanBoard() {
     const [tasks, setTasks] = useState([]);
 
+    const api = axios.create({
+        baseURL: "http://127.0.0.1:8000/kanbandata/",  // Без слеша в конце!
+    });
     useEffect(() => {
         fetch("http://127.0.0.1:8000/kanbandata/tasks/")
             .then(response => response.json())
@@ -33,7 +37,7 @@ export default function KanbanBoard() {
     };
 
     const handleDragEnd = (event: DragEndEvent) => {
-        const { active, over } = event;
+        const {active, over} = event;
 
         if (!over) return;
         // @ts-ignore
@@ -41,47 +45,72 @@ export default function KanbanBoard() {
         if (!activeTask) return;
 
 
-        if (over.id === 'todo' || over.id === 'in-progress' || over.id === 'done') {
+        if (over.id === 'todo' || over.id === 'in_progress' || over.id === 'done') {
             setTasks((tasks) =>
                 tasks.map((task) =>
-                    task.id === active.id ? { ...task, status: over.id as Task['status'] } : task
+                    task.id === active.id ? {...task, status: over.id as Task['status']} : task
                 )
             );
+                api.patch(`tasks/${activeTask.id}/`, {
+                    status: over.id
+                })
+                    .then(response => console.log("Статус успешно!", response))
+                    .catch(error => console.error("Ошибка:", error.response?.data || error.message));
+
             return;
         }
 
         // Обычное перемещение между задачами
         const overTask = tasks.find((task) => task.id === over.id);
+
+
         if (!overTask) return;
 
         if (activeTask.status !== overTask.status) {
             // Перемещение между колонками
             setTasks((tasks) =>
                 tasks.map((task) =>
-                    task.id === active.id ? { ...task, status: overTask.status } : task
+                    task.id === active.id ? {...task, status: overTask.status} : task
                 )
             );
+            api.patch(`tasks/${activeTask.id}/`, {
+                status: overTask.status
+            })
+                .then(response => console.log("Статус успешно!", response))
+                .catch(error => console.error("Ошибка:", error.response?.data || error.message));
         } else {
             // Перемещение внутри колонки
             const oldIndex = tasks.findIndex((task) => task.id === active.id);
             const newIndex = tasks.findIndex((task) => task.id === over.id);
             setTasks((tasks) => arrayMove(tasks, oldIndex, newIndex));
         }
+
     };
 
     const addTask = () => {
         if (!newTaskTitle.trim()) return;
 
+        api.post('tasks/', {
+            project: 1,
+            title: newTaskTitle,
+            description: "newTask.description",
+            status: 'todo',
+        })
+            .then(response => console.log("Добавление успешно!", response))
+            .catch(error => console.error("Ошибка:", error.response?.data || error.message));
+
         const newTask: Task = {
-            id: tasks.length + 1,
+            id: tasks.at(-1).id +1,
             title: newTaskTitle,
             status: 'todo',
         };
 
+
+
+
         setTasks([...tasks, newTask]);
         setNewTaskTitle('');
     };
-
     return (
         <div className="p-4">
             <div className="flex justify-between items-center mb-6">
@@ -99,7 +128,7 @@ export default function KanbanBoard() {
                         onClick={addTask}
                         className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 flex items-center gap-1"
                     >
-                        <PlusIcon className="h-5 w-5" />
+                        <PlusIcon className="h-5 w-5"/>
                         ADD
                     </button>
                 </div>
@@ -114,10 +143,10 @@ export default function KanbanBoard() {
                         isEmpty={getTasksByStatus('todo').length === 0}
                     />
                     <KanbanColumn
-                        id="in-progress"
+                        id="in_progress"
                         title="In Progress"
-                        tasks={getTasksByStatus('in-progress')}
-                        isEmpty={getTasksByStatus('in-progress').length === 0}
+                        tasks={getTasksByStatus('in_progress')}
+                        isEmpty={getTasksByStatus('in_progress').length === 0}
                     />
                     <KanbanColumn
                         id="done"
